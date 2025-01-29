6de26183ee27da67e4fa18edc676423e1ab89e12
TheFloatingBrain <1550298+TheFloatingBrain@users.noreply.github.com>
2025-01-28 22:02:24 -0500

    Major code file structure cleanup and I fixed that crash

---
+++ Include/BioGooContainmentSquadron/Goo.hpp
@@ -0,0 +1,53 @@
+#include <BioGooContainmentSquadron/Squaddie.hpp>
+#ifndef GOO_HEADER_HPP
+#define GOO_HEADER_HPP
+
+namespace BioGooContainmentSquad
+{
+	size_t RandomRange( int min, int max );
+
+	struct Goo : public Entity
+	{
+		using GooRefrence = std::reference_wrapper< Goo >;
+		Goo( entt::registry& registry_, BoardPosition start, 
+				GameBoard* board_, size_t tileWidth, size_t tileHeight );
+		~Goo();
+		struct GooComponent : Entity
+		{
+			Goo* parent;
+			GooComponent( entt::registry& registry_, BoardPosition start,
+					GameBoard* board_, size_t tileWidth, size_t tileHeight, Goo* parent_ );
+			void Update() override {}
+			void UpdateGraphics() override {}
+			void MakeGooAnimation();
+		};
+		using GooComponentRefrence = std::reference_wrapper< GooComponent >;
+		void Update() override {}
+		void UpdateGraphics() override {}
+
+		GooComponent& AddGoo( BoardPosition location );
+
+		bool RemoveGoo( GooRefrence& toRemove );
+		bool RemoveGoo( GooComponent& toRemove );
+		bool RemoveGoo( GooComponent* toRemove );
+		bool RemoveGoo( BoardPosition toRemove );
+		bool RemoveGoo( size_t toRemove );
+
+
+		using EmptySquareLocalType = std::vector< BoardPosition >;
+		using EmptySquareType = std::vector< std::pair< GooComponentRefrence, EmptySquareLocalType > >;
+
+		static EmptySquareLocalType FindEmptyAround( GooComponent& toLookAround, size_t spreadReach = 1 );
+		static EmptySquareType FindEmptySquares( Goo& goo, size_t spreadReach = 1 );
+
+		bool MoveToward( BoardPosition where, size_t spreadReach = 1, bool grow = false, std::vector< GooComponent* > exclude = {} );
+
+		GooComponent* ClosestToPoint( BoardPosition point, bool invert = false, std::vector< GooComponent* > exclude = {} );
+
+		std::vector< GooComponent* >& GetGoo();
+		protected:
+			std::vector< GooComponent* > goo;
+	};
+}
+
+#endif
---
+++ Include/BioGooContainmentSquadron/VectorUtility.hpp
@@ -0,0 +1,71 @@
+#include <BioGooContainmentSquadron/GameBoard.hpp>
+#include <math.h>
+
+#ifndef VECTOR_UTILITY_HEADER_HPP
+#define VECTOR_UTILITY_HEADER_HPP
+namespace BioGooContainmentSquad
+{
+
+	template< typename SCALER_TYPE = int >
+	constexpr sf::Rect< SCALER_TYPE > RectangleFromVectors( 
+			sf::Vector2< SCALER_TYPE > position, sf::Vector2< SCALER_TYPE > dimentions ) {
+		return sf::Rect< SCALER_TYPE >( position.x, position.y, dimentions.x, dimentions.y );
+	}
+
+	template< typename SCALER_0_TYPE, typename SCALER_1_TYPE >
+	constexpr sf::Rect< SCALER_0_TYPE > ConvertRectangle( sf::Rect< SCALER_1_TYPE > rectangle ) {
+		return sf::Rect< SCALER_0_TYPE >{ ( SCALER_0_TYPE ) rectangle.left, ( SCALER_0_TYPE ) rectangle.right, 
+				( SCALER_0_TYPE ) rectangle.width, ( SCALER_0_TYPE ) rectangle.height };
+	}
+
+	template< typename SCALER_TYPE = float >
+	constexpr double Magnitude( sf::Vector2< SCALER_TYPE > of ) {
+		return sqrt( ( of.x * of.x ) + ( of.y * of.y ) );
+	}
+
+	template< typename SCALER_TYPE = int >
+	constexpr double ConditionalMagnitude( sf::Vector2< SCALER_TYPE > of )
+	{
+		const auto ZERO_VECTOR = sf::Vector2< SCALER_TYPE >( ( SCALER_TYPE ) 0.0, ( SCALER_TYPE ) 0.0 );
+		if( of != sf::Vector2< SCALER_TYPE >( ( SCALER_TYPE ) 0.0, ( SCALER_TYPE ) 0.0 ) )
+			return Magnitude< SCALER_TYPE >( of );
+		return 0.0;
+	}
+
+	template< typename SCALER_0_TYPE, typename SCALER_1_TYPE >
+	constexpr sf::Vector2< SCALER_0_TYPE > ConvertVector( sf::Vector2< SCALER_1_TYPE > vector ) {
+		return sf::Vector2< SCALER_0_TYPE >{ ( SCALER_0_TYPE ) vector.x, ( SCALER_0_TYPE ) vector.y };
+	}
+
+	template< typename SCALER_TYPE >
+	constexpr sf::Vector2< SCALER_TYPE > RectanglePosition( sf::Rect< SCALER_TYPE > from ) {
+		return sf::Vector2< SCALER_TYPE >( from.left, from.top );
+	}
+
+	bool ComparePosition( sf::Vector2f first, sf::Vector2f second );
+
+	template< typename SCALER_TYPE = float >
+	struct ToUnitVector
+	{
+		sf::Vector2< SCALER_TYPE > result;
+		ToUnitVector( sf::Vector2< SCALER_TYPE > vector )
+		{
+			const double MAGNITUDE_CONSTANT = Magnitude( vector );
+			result = sf::Vector2< SCALER_TYPE >(
+				vector.x / ( SCALER_TYPE ) MAGNITUDE_CONSTANT, vector.y / ( SCALER_TYPE ) MAGNITUDE_CONSTANT );
+		}
+		operator sf::Vector2< SCALER_TYPE >() {
+			return result;
+		}
+	};
+
+	size_t ClosestFacing( sf::Vector2i vector );
+
+
+	void PrintRect( sf::FloatRect rect );
+	void PrintRect( sf::IntRect rect );
+	void PrintVect( sf::Vector2f v );
+	void PrintVect( sf::Vector2i v );
+
+}
+#endif
